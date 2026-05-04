7701e13aec1f53bb7c14091c6e06b0bbd9da08a3
Hemanth-Ponnurangam <133486142+Hemanth-Ponnurangam@users.noreply.github.com>
2026-05-04 18:27:41 +0530

    Update main.js

--- v2.2/js/admin/main.js
+++ v2.2/js/admin/main.js
@@ -6,7 +6,7 @@
 import { db } from '../shared/firebase.js';
 import { verifySuperAdmin } from '../shared/auth.js';
 import { esc, showAlert, showConfirm } from '../shared/dom.js';
-import { uploadPresetDB } from './csv.js';
+//import { uploadPresetDB } from './csv.js';
 
 let isSuperAdmin = false;
 
